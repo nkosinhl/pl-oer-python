40d91285a52599af642c686eaf095e53fe6ea4d5
Craig Zilles <zilles@illinois.edu>
2023-06-22 19:06:38 +0000

    valueOfStringAssignment: edit server.py

--- questions/valueOfStringAssignment/server.py
+++ questions/valueOfStringAssignment/server.py
@@ -1,5 +1,4 @@
 import random, copy, json
-from pythonHelper import *
 
 templates = [(1, 0, "y = {0}\nx = y\ny = {1}"),
              (2, 0, "x = {0}\ny = x\ny = {1}"), 
@@ -19,6 +18,11 @@ templates = [(1, 0, "y = {0}\nx = y\ny = {1}"),
              (16, 0, "y = {0}\nx = y\ny = {1}\ny = x"),
              (17, 1, "x = {0}\ny = {1}\nx = y\ny = x"),
 ]              
+
+def randomFruit(k = 1):
+    fruits = ["Apple", "Akee", "Apricot", "Avocado", "Banana", "Bilberry", "Blackberry", "Blackcurrant", "Blueberry", "Boysenberry", "Currant", "Cherry", "Cloudberry", "Coconut", "Cranberry", "Cucumber", "Damson", "Date", "Durian", "Elderberry", "Feijoa", "Fig", "Gooseberry", "Grape", "Raisin", "Grapefruit", "Guava", "Honeyberry", "Huckleberry", "Jabuticaba", "Jackfruit", "Jambul", "Jostaberry", "Jujube", "Kiwifruit", "Kumquat", "Lemon", "Lime", "Loquat", "Longan", "Lychee", "Mango", "Mangosteen", "Marionberry", "Melon", "Cantaloupe", "Honeydew", "Watermelon", "Mulberry", "Nectarine", "Nance", "Orange", "Clementine", "Mandarine", "Tangerine", "Papaya", "Passionfruit", "Peach", "Pear", "Persimmon", "Plantain", "Plum", "Pineapple", "Pineberry", "Pomegranate", "Pomelo", "Quince", "Raspberry", "Salmonberry", "Redcurrant", "Salak", "Satsuma", "Soursop", "Strawberry", "Tamarillo", "Tamarind", "Yuzu"]
+    selection = random.sample(fruits, k)
+    return selection[0] if k == 1 else selection
 
 def generate(data):
 
