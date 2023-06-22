d66831c41ddb73f699e15fadb9f746521a20c8dc
Craig Zilles <zilles@illinois.edu>
2023-06-22 20:33:12 +0000

    boolean_operators2: edit server.py

--- questions/boolean_operators2/server.py
+++ questions/boolean_operators2/server.py
@@ -1,5 +1,4 @@
 import random, copy, json
-from pythonHelper import *
 
 true_expressions = ["True and True or False", "True and False or True", "False and True or True", "False and False or True", "False or True and True", "True or False and True", "True or False and False", ]
 false_expressions = ["False or False and True", "False or True and False", "False and (True or False)", "True and False or False", "False and True or False", "(True or True) and False"]
