6401e17d8464eebb139baafb5f896e3f836920ac
Craig Zilles <zilles@illinois.edu>
2023-06-27 11:28:53 -0500

    update

--- questions/progCondPosOddCheck/tests/test.py
+++ questions/progCondPosOddCheck/tests/test.py
@@ -1,24 +1,41 @@
-import unittest, math, random, json, pltest
-from pltest import name, points
-from bin.student import posodd_number_filter
+import random
+from code_feedback import Feedback
+from pl_helpers import name, points
+from pl_unit_test import PLTestCase
 
-class Test(unittest.TestCase):
+def check_string(call_string, desired, actual):
+    if desired != actual:
+        Feedback.add_feedback(f"your {call_string} produced '{actual}' instead of '{desired}'")
+        return False
+    return True
+
+class Test(PLTestCase):
     @points(1)
     @name("Testing with 1")
     def test_one(self):
-        self.assertEquals(posodd_number_filter(1),"Accepted")
+        user_val = Feedback.call_user(self.st.posodd_number_filter, 1)
+        if check_string("posodd_number_filter(1)", 'Accepted', user_val): 
+            Feedback.set_score(1)
+        else:
+            Feedback.set_score(0)
 
     @points(1)
     @name("Testing with -5")
     def test_ten(self):
-        self.assertEquals(posodd_number_filter(-5),"Rejected")
+        user_val = Feedback.call_user(self.st.posodd_number_filter, -5)
+        if check_string("posodd_number_filter(-5)", 'Rejected', user_val): 
+            Feedback.set_score(1)
+        else:
+            Feedback.set_score(0)
 
     @points(8)
     @name("Testing with random numbers")
     def test_random(self):
         for i in range(100):
             x = random.randint(-1000, 1000)
-            if (x % 2 == 1) and (x > 0):
-                self.assertEquals(posodd_number_filter(x),"Accepted")
-            else:
-                self.assertEquals(posodd_number_filter(x),"Rejected")
+            expected = "Accepted" if (x % 2 == 1) and (x > 0) else "Rejected"
+            user_val = Feedback.call_user(self.st.posodd_number_filter, x)
+            if not check_string(f"posodd_number_filter({x})", expected, user_val): 
+                Feedback.set_score(0)
+                return
+        Feedback.set_score(1)
--- questions/progCondReturnString/server.py
+++ questions/progCondReturnString/server.py
@@ -3,7 +3,7 @@ def generate(data):
     data["params"]["names_from_user"] = [
         {
             "name": "string_indicating_negativity",
-            "description": "Function to compute the $n^\\text{th}$ Fibonacci number",
+            "description": "Function to return string based on whether given number is negative or not",
             "type": "python function",
         }
     ]
