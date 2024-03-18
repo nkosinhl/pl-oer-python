66efec6e085096ca94a1254e603443f1afa1c103
Mariana Silva <mfsilva@illinois.edu>
2024-03-17 20:51:33 -0700

    fixed progCondPosOddCheck

---
+++ questions/progCondPosOddCheck/server.py
@@ -0,0 +1,9 @@
+def generate(data):
+    data["params"]["names_for_user"] = []
+    data["params"]["names_from_user"] = [
+        {
+            "name": "posodd_number_filter",
+            "description": "",
+            "type": "python function",
+        }
+    ]
